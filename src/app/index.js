5d4e9017b8917606c059150f9348ffa1f3bdc080
Tom Hopkins <tom@githop.com>
2015-06-26 03:00:06 -0600

    add TODOs

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+//TODO need to add auth interceptor
+//TODO setup auth roles
+//TODO login / logout
+//TODO add restricted states for create, edit posts
+//TODO figure out published / unpublished posts
+
 angular.module('githopwww', [
   'ngAnimate', 'ngCookies',
   'ngTouch', 'ngSanitize',
